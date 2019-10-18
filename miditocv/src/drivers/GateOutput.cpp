1b168a053ac1381115ce66c31633689b53b1b504
rob <rob@pigatron-industries.co.uk>
2019-10-18 08:08:49 +0100

    Gate outputs

---
+++ miditocv/src/drivers/GateOutput.cpp
@@ -0,0 +1,17 @@
+#include "GateOutput.h"
+
+#include <Arduino.h>
+
+GateOutput::GateOutput(uint8_t dataPin, uint8_t latchPin, uint8_t clockPin, int size) :
+        _latchPin(latchPin),
+        _clockPin(clockPin),
+        _dataPin(dataPin),
+        _size(size) {
+    pinMode(_dataPin, OUTPUT);
+    pinMode(_latchPin, OUTPUT);
+    pinMode(_clockPin, OUTPUT);
+    digitalWrite(_clockPin, LOW);
+    digitalWrite(_latchPin, LOW);
+    digitalWrite(_dataPin, LOW);
+    _data = new bool[_size];
+}
