22179cc342b92aa01349caff3ab7dd90c4b12077
rob <rob@pigatron-industries.co.uk>
2019-10-20 11:27:44 +0100

    Handle system messages

--- miditocv/src/tasks/MidiInputTask.cpp
+++ miditocv/src/tasks/MidiInputTask.cpp
@@ -4,8 +4,16 @@
 #include <Arduino.h>
 
 
-#define COMMAND_NOTEOFF 8
-#define COMMAND_NOTEON 9
+#define COMMAND_NOTEOFF 0x8
+#define COMMAND_NOTEON 0x9
+#define COMMAND_POLY_PRESSURE 0xA
+#define COMMAND_CONTROL_CHANGE 0xB
+#define COMMAND_PROGRAM_CHANGE 0xC
+#define COMMAND_CHAN_PRESSURE 0xD
+#define COMMAND_PITCH_BEND 0xE
+#define COMMAND_SYSTEM 0xF
+
+#define SYSTEM_CLOCK 0x8
 
 
 
@@ -26,32 +34,42 @@ void MidiInputTask::execute() {
     while(Serial2.available()) {
         byte byte1 = Serial2.read();
         if(byte1 >= 0x80) {
-            while(!Serial2.available()){}
-            byte byte2 = Serial2.read();
-            while(!Serial2.available()){}
-            byte byte3 = Serial2.read();
 
             byte command = HI_NYBBLE(byte1);
             byte channel = LO_NYBBLE(byte1);
 
-            // Serial.println("");
-            // Serial.println("Command");
-            // Serial.println(command);
-            // Serial.println("Channel");
-            // Serial.println(channel);
-            // Serial.println("Data 1");
-            // Serial.println(byte2);
-            // Serial.println("Data 2");
-            // Serial.println(byte3);
+            if(command != COMMAND_SYSTEM) {
 
-            if(command == COMMAND_NOTEON) {
-                _midiEventProcessor.eventNoteOn(channel, byte2, byte3);
-            }
+                while(!Serial2.available()){}
+                byte byte2 = Serial2.read();
+                while(!Serial2.available()){}
+                byte byte3 = Serial2.read();
 
-            //TODO if pressure is reduced to zero then treat as note off
+                // Serial.println("");
+                // Serial.println("Command");
+                // Serial.println(command);
+                // Serial.println("Channel");
+                // Serial.println(channel);
+                // Serial.println("Data 1");
+                // Serial.println(byte2);
+                // Serial.println("Data 2");
+                // Serial.println(byte3);
 
-            if(command == COMMAND_NOTEOFF) {
-                _midiEventProcessor.eventNoteOff(channel, byte2);
+                if(command == COMMAND_NOTEON) {
+                    // Serial.println("Note on ");
+                    // Serial.println(byte2);
+                    _midiEventProcessor.eventNoteOn(channel, byte2, byte3);
+                }
+
+                if(command == COMMAND_NOTEOFF || (command == COMMAND_POLY_PRESSURE && byte3 == 0)) {
+                    _midiEventProcessor.eventNoteOff(channel, byte2);
+                }
+
+            } else { // command == COMMAND_SYSTEM
+
+                // if(channel == SYSTEM_CLOCK) {
+                //     //Serial.println("clock");
+                // }
             }
         }
     }
