455e5193a4fc15d2fe4c5a2324c55a5ed81c94dc
Robert Ellis <robert.ellis@rightmove.co.uk>
2019-10-29 13:19:35 +0000

    removed unused defines

--- miditocv/src/processor/MidiToPitchConverter.cpp
+++ miditocv/src/processor/MidiToPitchConverter.cpp
@@ -3,9 +3,6 @@
 #include <Arduino.h>
 #include "math.h"
 
-#define SCALE_NOTESPEROCTAVE 12
-#define SCALE_OCTAVECENTS 1200
-#define SCALE_NOTECENTS 100
 
 MidiToPitchConverter::MidiToPitchConverter(Configuration& config) :
         _config(config) {
