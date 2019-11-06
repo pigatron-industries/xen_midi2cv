5c13727ec7b9d0a0a5da027fa0cb326f929a4eb1
Robert Ellis <robert.ellis@rightmove.co.uk>
2019-11-06 09:11:40 +0000

    Changed pins for trigger output

--- miditocv/src/hwconfig.h
+++ miditocv/src/hwconfig.h
@@ -4,10 +4,9 @@
 #define GATE_LATCH_PIN pinNametoDigitalPin(PD_6)
 #define GATE_CLOCK_PIN pinNametoDigitalPin(PD_5)
 
-// TODO define trigger pins
-#define TRIG_DATA_PIN pinNametoDigitalPin(PD_7)
-#define TRIG_LATCH_PIN pinNametoDigitalPin(PD_6)
-#define TRIG_CLOCK_PIN pinNametoDigitalPin(PD_5)
+#define TRIG_DATA_PIN pinNametoDigitalPin(PE_6)
+#define TRIG_LATCH_PIN pinNametoDigitalPin(PE_3)
+#define TRIG_CLOCK_PIN pinNametoDigitalPin(PE_8)
 
 #define PITCHCV_DATA_PIN pinNametoDigitalPin(PE_2)
 #define PITCHCV_CLOCK_PIN pinNametoDigitalPin(PE_4)
